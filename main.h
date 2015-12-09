e3c8bb5a8a41b50e7d8dd02e754a52dd006442b2
Mykhaylo Berdar <berdarm@gmail.com>
2015-12-09 23:14:43 +0200

    added main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void swap(double*, double*);
+int partition(double*, int, int, int);
+void quickSort(double*, int, int);
+double *quickSort_p(MPI_Comm comm, double *local_array, int local_size, int *arrayLength);
+
+void divide(int*, int*, double, double*, int);
+int validateSort(double *a, int size);
